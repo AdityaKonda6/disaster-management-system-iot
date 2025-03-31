54f1e998444292a726db03c1e2b110889424c6ab
Aditya Konda <adityakonda04@gmail.com>
2025-03-31 15:45:07 +0530

    modified:   connection.php

--- connection.php
+++ connection.php
@@ -1,24 +1,19 @@
 <?php
-$user = "root";
-$db = "disaster";
-$password = "";
-$server = "localhost";
+$server = "sql304.infinityfree.com";  // Your MySQL Host
+$user = "if0_38642133";  // Your MySQL Username
+$password = "Adityakonda2003";  // Your MySQL Password
+$db = "if0_38642133_disaster";  // Your Database Name
+$port = 3306;  // Default MySQL Port
 
-$link = mysqli_connect($server,$user,$password,$db);
-if(!$link)
-{
-		 echo "<script>
-		 alert('Unable to connect to Database Contact your Administrator ');
-			window.location.href='index.php';
-			
-			</script>";
-			die();
-}
-else
-{
-	//echo "connection successful";
-}
+$link = mysqli_connect($server, $user, $password, $db, $port);
 
-date_default_timezone_set("Asia/calcutta");
+if (!$link) {
+    echo "<script>
+        alert('Unable to connect to Database. Contact your Administrator.');
+        window.location.href='index.php';
+    </script>";
+    die();
+} 
 
+date_default_timezone_set("Asia/Kolkata");
 ?>
